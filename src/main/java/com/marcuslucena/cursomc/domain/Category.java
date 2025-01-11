73c432d796b205f360d6f9850393d05d71d5a724
MarcusLucena <marcus-lucena@outook.com>
2025-01-10 23:58:28 -0300

    Added database

--- src/main/java/com/marcuslucena/cursomc/domain/Category.java
+++ src/main/java/com/marcuslucena/cursomc/domain/Category.java
@@ -1,12 +1,22 @@
 package com.marcuslucena.cursomc.domain;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
